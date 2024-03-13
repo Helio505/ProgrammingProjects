72fda28f4b5e338464bf56ce4485c25eef8e1c00
Helio Soares <heliob_18@hotmail.com>
2024-03-12 23:41:09 -0300

    CalculadoraMediaNotas commit inicial

---
+++ vanilla-JavaScript-HTML-CSS/CalculadoraMediaNotas/README.md
@@ -0,0 +1,16 @@
+# Calculadora de média de notas
+
+## Descrição
+
+Calculadora de média de notas feita em HTML, CSS e JavaScript. Utiliza event listeners e forms.
+
+## Uso
+
+1. Insere nome.
+2. Insere cada nota.
+3. Clica em calcular.
+4. Visualiza o resultado.
+
+## Print
+
+![alt text](image.png)
---
+++ vanilla-JavaScript-HTML-CSS/CalculadoraMediaNotas/script.js
@@ -0,0 +1,33 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Pegando os valores dos inputs
+  const nome = document.getElementById("nome").value;
+  const nota1 = parseFloat(document.getElementById("nota1").value);
+  const nota2 = parseFloat(document.getElementById("nota2").value);
+  const nota3 = parseFloat(document.getElementById("nota3").value);
+
+  // Tratando nome
+  // Capitalizando a primeira letra. Para cada primeira letra.
+  const separadas = nome.split(" ");
+  let nomeCapitalizado = "";
+  separadas.map((palavra) => {
+    nomeCapitalizado +=
+      palavra.charAt(0).toUpperCase() + palavra.slice(1) + " ";
+  });
+
+  // Calculo
+  const media = ((nota1 + nota2 + nota3) / 3).toFixed(2);
+  let resultado = document.getElementById("resultado");
+
+  // Handle resultado do calculo
+  if (media >= 7) {
+    resultado.style.border = "1px solid";
+    resultado.style.color = "green";
+    resultado.innerHTML = ` ${nomeCapitalizado}: APROVADO(a) com média ${media}`;
+  } else {
+    resultado.style.border = "1px solid";
+    resultado.style.color = "red";
+    resultado.innerHTML = ` ${nomeCapitalizado}: REPROVADO(a) com média ${media}`;
+  }
+});
